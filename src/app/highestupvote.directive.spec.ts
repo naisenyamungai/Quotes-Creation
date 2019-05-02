2193722b5f900683db97205d2aa58d55aa40b775
naisenyamungai <naisenyamungai@gmail.com>
2019-05-02 13:22:07 +0300

    create custom directiveto highlight the quote that has the highest upvotes

---
+++ src/app/highestupvote.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighestupvoteDirective } from './highestupvote.directive';
+
+describe('HighestupvoteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighestupvoteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/highestupvote.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHighestupvote]'
+})
+export class HighestupvoteDirective {
+
+  constructor() { }
+
+}
