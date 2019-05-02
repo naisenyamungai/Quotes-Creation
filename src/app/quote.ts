2feb55bc1bf49b43991080717033909a3d8de630
naisenyamungai <naisenyamungai@gmail.com>
2019-05-02 17:15:00 +0300

    modify .css files and add background image. Also modify contents of quote-details inorder to display author details

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {
-    constructor(public id:number, public name:string, public quote:string, public submitter:string){}
+    constructor(public id:number, public author:string, public quote:string, public submitter:string){}
 }
