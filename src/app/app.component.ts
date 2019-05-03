0c62f5aadf5468682d628280ada1a1265dccd804
naisenyamungai <naisenyamungai@gmail.com>
2019-05-03 12:25:12 +0300

    create dateCountPipe and add it's logic in date-count.pipe.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,5 @@ import { Quote} from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes = [
-    new Quote(1,'Mother Theresa', 'If you Judge People, you have no time to Love Them', 'Naisenya'),
-    new Quote(2,'Oprah Winfrey', 'Surround yourself with only people who are going to lift you higher', 'Naisenya'),
-    new Quote(3,'Maya Angelou', 'When you learn, teach. When you get, give', 'Naisenya'),
-  ]
+ 
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     public showAuthor:boolean;
-    constructor(public id:number, public author:string, public quote:string, public submitter:string){
+    constructor(public id:number, public author:string, public quote:string, public submitter:string, public completeDate:Date){
        this.showAuthor = false
    }
 }
