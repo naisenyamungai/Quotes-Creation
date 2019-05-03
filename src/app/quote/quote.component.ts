91a8da5b10905f0a606d6d853e289892041eb92d
naisenyamungai <naisenyamungai@gmail.com>
2019-05-03 16:09:15 +0300

    add styling to the header and footer sections

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -16,6 +16,13 @@ export class QuoteComponent implements OnInit {
     new Quote(5,'Miguna Miguna', 'Am not Boarding','Naisenya', new Date(2019, 4, 3) )
   ]
 
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+
   deleteQuote(isComplete,index){
     if(isComplete){
       let toDelete = confirm('Are you sure you want to delet ${this.goals[index].name}')
