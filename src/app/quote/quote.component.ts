1cfad22167b2d2ddd6d3a68216be4d7282adea02
naisenyamungai <naisenyamungai@gmail.com>
2019-05-03 13:29:57 +0300

    add delete button, style the button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {Quote} from '../quote';
 
 @Component({
@@ -9,12 +9,22 @@ import {Quote} from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes = [
-    new Quote(1,'Mother Theresa', 'If you Judge People, you have no time to Love Them', 'Naisenya', new Date(2019, 3, 1) ),
-    new Quote(2,'Oprah Winfrey', 'Surround yourself with only people who are going to lift you higher', 'Naisenya', new Date(2019, 2, 20) ),
-    new Quote(3,'Maya Angelou', 'When you learn, teach. When you get, give', 'Naisenya', new Date(2019, 2, 7) ),
-    new Quote(4,'Wavinya Ndeti', 'Yaliyo Ndwele Sipite', 'Naisenya', new Date(2019, 3, 13) ),
-    new Quote(5,'Miguna Miguna', 'Am not Boarding','Naisenya', new Date(2019, 2, 17) )
+    new Quote(1,'Mother Theresa', 'If you Judge People, you have no time to Love Them', 'Naisenya', new Date(2019, 4, 3) ),
+    new Quote(2,'Oprah Winfrey', 'Surround yourself with only people who are going to lift you higher', 'Naisenya', new Date(2019, 4, 3) ),
+    new Quote(3,'Maya Angelou', 'When you learn, teach. When you get, give', 'Naisenya', new Date(2019, 4, 3) ),
+    new Quote(4,'Wavinya Ndeti', 'Yaliyo Ndwele Sipite', 'Naisenya', new Date(2019, 4, 3) ),
+    new Quote(5,'Miguna Miguna', 'Am not Boarding','Naisenya', new Date(2019, 4, 3) )
   ]
+
+  deleteQuote(isComplete,index){
+    if(isComplete){
+      let toDelete = confirm('Are you sure you want to delet ${this.goals[index].name}')
+        if(toDelete){
+          this.quotes.splice(index,1)
+        }
+    }
+  }
+
   toggleDetails(index){
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
   }
